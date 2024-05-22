9b492bda4a33b7f93ff3b2d5fbc6063fa7cfb880
VINICIUS HONORIO YAZBEK <202301095701@alunos.ibmec.edu.br>
2024-05-22 09:44:28 -0300

    adicionando uma logica

--- projeto-musica1/src/main/java/com/mycompany/projeto/musica1/ProjetoMusica1.java
+++ projeto-musica1/src/main/java/com/mycompany/projeto/musica1/ProjetoMusica1.java
@@ -4,15 +4,21 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 class Aula {
-    String titulo;
-    String descricao;
-    int duracao;
+    private String titulo;
+    private String descricao;
+    private int duracao;
+    
+
 
     // Construtor
     public Aula(String titulo, String descricao, int duracao) {
         this.titulo = titulo;
         this.descricao = descricao;
         this.duracao = duracao;
+    }
+    
+    public String getName() {
+        return titulo;
     }
 
     // Métodos getters e setters
@@ -24,29 +30,59 @@ public class ProjetoMusica1 {
     public static void main(String[] args) {
         // Criando um ArrayList para armazenar as aulas
         ArrayList<Aula> aulas = new ArrayList<>();
-
-        // Criando um Scanner para ler a entrada do usuário
-        Scanner scanner = new Scanner(System.in);
-
-        System.out.println("Digite o título da aula:");
-        String tituloAula = scanner.nextLine();
-
-        System.out.println("Digite a descrição da aula:");
-        String descricaoAula = scanner.nextLine();
-
-        System.out.println("Digite a duração da aula (em minutos):");
-        int duracaoAula = scanner.nextInt();
-
-        // Criando uma aula
-        Aula aula = new Aula(tituloAula, descricaoAula, duracaoAula);
-
-        // Adicionando a aula ao ArrayList
-        aulas.add(aula);
-
-        // Imprimindo as informações da aula
-        System.out.println("Aula criada com sucesso!");
-        System.out.println("Título: " + aula.titulo);
-        System.out.println("Descrição: " + aula.descricao);
-        System.out.println("Duração: " + aula.duracao + " minutos");
+        
+        Aula aula1 = new Aula("Aula 1","asasas",2);
+        Aula aula2 = new Aula("Aula 2","asasas",2);
+        
+        aulas.add(aula1);
+        aulas.add(aula2);
+        
+        Usuario Vinicius = new Usuario("Vinicius","vini24","1234");
+        
+        Usuario Pinicius = new Usuario("Vinicius","vini24","1234");
+        
+        Vinicius.addAula(aula1);
+        Pinicius.addAula(aula2);
+        
+        Vinicius.listar();
+        
+        String nome = "";
+        String descricao = "";
+        int duracao = 0;
+       
+        int teste = 0;
+        while (teste == 0) {
+            System.out.println("1- Adicionar Aula 2- Adicionar Aluno 3- Listar Aula 4- Listar Alunos 5- Login 6- Sair");
+                                
+            Scanner scanner = new Scanner(System.in);
+            int cursor = scanner.nextInt();
+            
+            if (cursor >= 7 || cursor <= 0) {
+                System.out.println("Tente De novo");
+            }
+            
+            if (cursor == 6) {
+                teste = 1;
+            }
+            
+            if (cursor == 1) {
+                System.out.println("Escreve o Titulo");
+                nome = scanner.next();
+                
+                System.out.println("Descreve a aula");
+                descricao = scanner.next();
+                
+                System.out.println("Da a Duracao");
+                duracao = scanner.nextInt();
+  
+                aulas.add(new Aula(nome,descricao,duracao));
+            }
+            
+            if (cursor == 3) {
+                for (int x = 0 ; x < aulas.size() ; x++) {
+                    System.out.println(aulas.get(x).getName());
+                }
+            }
+        }
     }
 }
